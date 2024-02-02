825d36cd5be0c346dd0614924c33e2598a4e4d97
Akilz02 <cb011250@students.apiit.lk>
2024-02-02 20:02:14 +0530

    user and menu crud - initial

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,13 +15,18 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
-
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="/">
+                    <x-nav-link href="/" :active="request()->routeIs('/')">
                         {{ __('Home') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('user.index') }}" :active="request()->routeIs('user.*')">
+                        {{ __('Users') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('menu-category.index') }}" :active="request()->routeIs('menu-category.*')">
+                        {{ __('Categories') }}
+                    </x-nav-link>
                 </div>
+
+
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -77,6 +82,58 @@
                     </div>
                 @endif
 
+                @auth
+                <!-- Settings Dropdown -->
+                <div class="ms-3 relative">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                                <span class="inline-flex rounded-md">
+                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                        Administration
+
+                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </button>
+                                </span>
+                        </x-slot>
+
+                        <x-slot name="content">
+
+                            <!-- Account Management -->
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                {{ __('Manage Users') }}
+                            </div>
+
+                            <x-dropdown-link href="{{route('user.index')}}">
+                                {{ __('Users') }}
+                            </x-dropdown-link>
+
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                {{ __('Manage Products') }}
+                            </div>
+
+                             <x-dropdown-link href="{{route('menu-category.index')}}"> 
+                                {{ __('Categories') }}
+                            </x-dropdown-link>
+
+                            <div class="border-t border-gray-200"></div>
+
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}" x-data>
+                                @csrf
+
+                                <x-dropdown-link href="{{ route('logout') }}"
+                                         @click.prevent="$root.submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+                @endauth
+
+                @auth
                 <!-- Settings Dropdown -->
                 <div class="ms-3 relative">
                     <x-dropdown align="right" width="48">
@@ -128,7 +185,10 @@
                         </x-slot>
                     </x-dropdown>
                 </div>
+                @endauth
+
             </div>
+            
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
